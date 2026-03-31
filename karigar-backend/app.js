f3ab75a32e1d25198eafb38bd171e71df680f771
Yogi526 <yogendarpanchal284@gmail.com>
2026-03-31 10:03:51 +0530

    Set up Express server with MongoDB connection

--- karigar-backend/app.js
+++ karigar-backend/app.js
@@ -1,1 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connect
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected!'))
+  .catch(err => console.log(err));
+
+// Routes
+app.use('/auth', require('./routes/auth'));
+app.use('/workers', require('./routes/workers'));
+
+app.get('/', (req, res) => {
+  res.send('Karigar API running!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
